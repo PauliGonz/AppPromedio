63753f1fd695bcf804e76b6a6f855c3fd167a4ad
Pauli Gonz <andreagrafic79@gmail.com>
2023-10-16 20:39:39 -0300

    "cambio de operacion a suma y resta"

--- app/src/main/java/com/example/apppromedio/MainActivity.java
+++ app/src/main/java/com/example/apppromedio/MainActivity.java
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1;
-    private EditText et2;
-    private EditText et3;
+    private EditText et1, et2;
     private TextView tv1;
+    private RadioButton rb1,rb2;
 
 
     @Override
@@ -20,27 +20,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText)findViewById(R.id.txt_matematicas);
-        et2 = (EditText)findViewById(R.id.txt_fisica);
-        et3 = (EditText)findViewById(R.id.txt_quimica);
-        tv1 = (TextView)findViewById(R.id.tv_status);
+        et1 = (EditText)findViewById(R.id.txt_valor1);
+        et2 = (EditText)findViewById(R.id.txt_valor2);
+        tv1 = (TextView)findViewById(R.id.txt_resultado);
+        rb1 = (RadioButton)findViewById(R.id.rb_sumar);
+        rb2 = (RadioButton)findViewById(R.id.rb_restar);
     }
 
-    public void status(View view){
-        String matematicas_String = et1.getText().toString();
-        String fisica_String = et2.getText().toString();
-        String quimica_String = et3.getText().toString();
+    public void Calcular(View view){
+        String valor1_String = et1.getText().toString();
+        String valor2_String = et2.getText().toString();
 
-        int Matematicas_int = Integer.parseInt(matematicas_String);
-        int Fisica_int = Integer.parseInt(fisica_String);
-        int Quimica_int = Integer.parseInt(quimica_String);
+        int valor1_int = Integer.parseInt(valor1_String);
+        int valor2_int = Integer.parseInt(valor2_String);
 
-        int promedio = (Matematicas_int + Fisica_int + Quimica_int)/3;
-
-        if(promedio >=6){
-            tv1.setText("Status Aprobado con " + promedio);
-        }else if(promedio <=5){
-            tv1.setText("Status Reprobado con " + promedio);
+        if(rb1.isChecked() == true){
+            int suma = valor1_int + valor2_int;
+            String resultado = String.valueOf(suma);
+            tv1.setText(resultado);
+        }else if(rb2.isChecked() == true){
+            int resta = valor1_int - valor2_int;
+            String resultado = String.valueOf(resta);
+            tv1.setText(resultado);
         }
     }
 }
